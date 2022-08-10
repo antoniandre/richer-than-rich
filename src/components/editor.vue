5f470157fd531422a53a3d28c8653fb881f3ecc1
antoniandre <antoniandre.web@gmail.com>
2022-08-10 21:03:18 +0200

    Add all the available buttons with icons.

--- src/components/editor.vue
+++ src/components/editor.vue
@@ -8,10 +8,31 @@ const props = defineProps({
 
 let inputField = ref(null)
 const buttons = {
-  bold: { code: 'b', tag: 'strong', label: 'b' },
-  italic: { code: 'i', tag: 'em', label: 'i' },
-  underline: { code: 'u', tag: 'span', label: 'u' },
-  strikethrough: { code: 's', tag: 'span', label: 's' }
+  bold: { code: 'b', tag: 'strong', label: 'Bold' },
+  italic: { code: 'i', tag: 'em', label: 'Italic' },
+  underline: { code: 'u', tag: 'span', label: 'Underline' },
+  strikethrough: { code: 's', tag: 'span', label: 'Strikethrough' },
+  'strikethrough-1': { code: 's', tag: 'span', label: 'Strikethrough' },
+  'align-left': { code: '|<', tag: 'span', label: 'Align left' },
+  'align-center': { code: '|-|', tag: 'span', label: 'Align center' },
+  'align-right': { code: '>|', tag: 'span', label: 'Align right' },
+  'align-justify': { code: '||', tag: 'span', label: 'Align justify' },
+  indent: { code: '>', tag: 'span', label: 'indent' },
+  unindent: { code: '<', tag: 'span', label: 'Unindent' },
+  'font-size': { code: '||', tag: 'span', label: 'Text size' },
+  'text-color': { code: '||', tag: 'span', label: 'Text color' },
+  subscript: { code: '_', tag: 'span', label: 'Subscript' },
+  superscript: { code: 'º', tag: 'span', label: 'Superscript' },
+  code: { code: '</>', tag: 'span', label: 'Code' },
+  link: { code: '∞', tag: 'span', label: 'Link' },
+  'link-1': { code: '∞', tag: 'span', label: 'Link' },
+  'list-ul': { code: '-', tag: 'span', label: 'Bulleted list' },
+  'list-ol': { code: '.', tag: 'span', label: 'Numbered list' },
+  table: { code: '[]', tag: 'span', label: 'Table' },
+  undo: { code: '<-', tag: 'span', label: 'Undo' },
+  redo: { code: '->', tag: 'span', label: 'Redo' },
+  image: { code: '*', tag: 'span', label: 'Photo' },
+  'clear-format': { code: 'x', tag: 'span', label: 'Clear format' },
 }
 const menuButtons = ref([])
 const placeholder = ref(props.placeholder)
@@ -81,8 +102,9 @@ const focus = () => {
       :key="i"
       @click="action(button)"
       type="button"
-      :class="{ [`button--${button.name} ${button.name}`]: true, 'button--active': button.active }").
-      {{ button.label }}
+      :title="button.label"
+      :class="{ [`button--${button.name} i-${button.name}`]: true, 'button--active': button.active }")
+      span {{ button.label }}
   .content-wrap
     .richer__content(
       ref="inputField"
@@ -114,7 +136,7 @@ $highlight-color: #bf953f;
     border-top-right-radius: inherit;
     border-bottom: 1px solid rgba(#fff, 0.05);
     background-color: rgba(#000, 0.05);
-    padding: 4px 8px;
+    padding: 4px 6px;
     display: flex;
     flex-wrap: wrap;
     align-items: center;
@@ -130,15 +152,13 @@ $highlight-color: #bf953f;
       font-size: 1rem;
       border: 1px solid rgba(#000, 0.1);
       border-radius: 4px;
-      margin-left: 4px;
+      margin: 2px;
       cursor: pointer;
       background-color: transparent;
       transition: background-color 0.2s ease-in-out;
       -webkit-tap-highlight-color: transparent;
       user-select: none;
       transition: 0.15s ease-in-out;
-
-      &:first-child {margin-left: 0;}
 
       &:hover, &--active {border-color: rgba(#000, 0.06);}
       &:focus {
@@ -147,7 +167,7 @@ $highlight-color: #bf953f;
       }
       &:active {border-color: rgba(#000, 0.06);}
 
-      &:before {
+      &:after {
         content: '';
         position: absolute;
         top: 0;
@@ -161,11 +181,13 @@ $highlight-color: #bf953f;
         transition: inherit;
       }
 
-      &:hover:before, &:active:before, &--active:before {
+      &:hover:after, &:active:after, &--active:after {
         opacity: 1;
         transform: scale(1);
         border-radius: 3px;
       }
+
+      span {display: none;}
     }
   }
 
@@ -221,7 +243,7 @@ $highlight-color: #bf953f;
       }
       &:active {border-color: rgba(#fff, 0.06);}
 
-      &:before {background-color: rgba(#fff, 0.06);}
+      &:after {background-color: rgba(#fff, 0.06);}
     }
   }
 }
