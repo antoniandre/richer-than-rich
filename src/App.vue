f08c559ac901682e70573ef43d6ab0fdc5af2a83
antoniandre <antoniandre.web@gmail.com>
2022-08-13 11:43:42 +0200

    Restructure the app into more components.

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,7 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import Editor from './components/editor.vue'
-
-let darkTheme = ref(true)
-const editorContent = ref('<div style="text-align: center"><br><h2>Hi. I\'m r$$$ch.</h2><p><br><br>Wanna try me?</p></div>')
-
-const cl = (...args) => console.log(...args)
-
-onMounted(() => {
-  darkTheme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
-})
+import documentation from '@/documentation/index.vue'
 </script>
 
 <template lang="pug">
-.page(:class="darkTheme ? 'dark' : 'light'")
-  .top-bar
-    .theme-switch(@click="darkTheme = !darkTheme")
-      button.button.i-moon(:class="{ 'button--active': darkTheme }")
-      button.button.i-sun(:class="{ 'button--active': !darkTheme }")
-      .slider
-  header
-    .container
-      h1
-        | Richer than you
-        sup.dollar $
-      p.catchphrase $. Another modern rich text editor. This one's richer than you.
-
-      editor.hero(
-        v-model="editorContent"
-        placeholder="Write something..."
-        :dark-mode="darkTheme"
-        @button-click="cl('button-click', $event)"
-        @click="cl('click', $event)"
-        @keyup="cl('keyup', $event)"
-        @focus="cl('focus', $event)"
-        @blur="cl('blur', $event)"
-        @paste="cl('paste', $event)")
-
-  main
-    .container
-      ul.checklist
-        li Designed for Vue 3
-        li Designed for light and dark darkThemes
-        li Fully accessible
-        li Lightweight
-        li Modern technics using recent browsers powers. No legacy-IE-code.
-        li Modular
-        li Clean HTML output
-        li Plug and play
-        li Responsive
-
-.particles
-  - for (i = 1; i <= 100; i++)
-    .c
+documentation
 </template>
---
+++ src/documentation/components/top-bar.vue
@@ -0,0 +1,69 @@
+<script setup>
+defineProps({
+  darkTheme: { Boolean }
+})
+
+defineEmits(['update:darkTheme'])
+</script>
+
+<template lang="pug">
+.top-bar
+  .theme-switch(@click="$emit('update:darkTheme', !darkTheme)")
+    button.button.i-moon(:class="{ 'button--active': darkTheme }")
+    button.button.i-sun(:class="{ 'button--active': !darkTheme }")
+    .slider
+</template>
+
+<style lang="scss">
+.top-bar {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 48px;
+}
+
+.theme-switch {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  height: 26px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 2px;
+  border-radius: 99rem;
+  line-height: 0;
+  background-color: rgba(#bf953f, 0.5);
+  box-shadow: 1px 1px 3px #973 inset;
+  cursor: pointer;
+
+  .button {
+    display: inline-flex;
+    font-size: 18px;
+    line-height: 1;
+    margin: 2px;
+    border: none;
+    background: none;
+    z-index: 1;
+    color: rgba(#fff, 1);
+    cursor: pointer;
+    transition: 0.3s ease-in-out;
+
+    &--active {color: #bf953f;}
+  }
+
+  .slider {
+    position: absolute;
+    border-radius: 99rem;
+    left: 2px;
+    height: 22px;
+    width: 22px;
+    background-color: rgba(#fff, 1);
+    box-shadow: 0 0 3px rgba(#000, 0.15);
+    transition: 0.3s ease-in-out;
+  }
+
+  .i-sun.button--active ~ .slider {transform: translateX(100%);}
+}
+</style>
