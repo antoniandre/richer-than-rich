10ac558218cb68352056fb3e5b60f30cd6bc1ad6
antoniandre <antoniandre.web@gmail.com>
2022-08-19 11:09:21 +0200

    Add Vite config for building and bundling.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,28 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 
+const bundlingConf = {
+  lib: {
+    entry: resolve(__dirname, '/src/richer-than-you/index.js'),
+    name: 'RicherThanYou',
+    formats: ['es', 'umd', 'cjs']
+  },
+  rollupOptions: {
+    plugins: [
+      // Rollup generates all the files, then remove what we don't want.
+      // Delete({ targets: ['dist/{images}', 'dist/*.{ico,txt,html}'], hook: 'generateBundle' })
+    ],
+    // Make sure to externalize deps that shouldn't be bundled into library.
+    external: ['vue'],
+    output: {
+      // Provide global variables to use in the UMD build for externalized deps.
+      globals: { vue: 'Vue' }
+    }
+  }
+}
+
+const build = process.env.BUNDLE ? bundlingConf : { outDir: 'docs' }
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -16,5 +38,6 @@ export default defineConfig({
         additionalData: '@import "@/documentation/scss/variables";'
       }
     }
-  }
+  },
+  build
 })
