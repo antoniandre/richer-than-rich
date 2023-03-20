752f53f01465916c24eff40198456e2ea5ec0963
antoniandre <antoniandre.web@gmail.com>
2023-03-20 12:43:20 +0100

    Expose the current version.

--- src/documentation/components/top-bar.vue
+++ src/documentation/components/top-bar.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { version } from  '/package.json'
+
 const props = defineProps({
   darkMode: Boolean
 })
@@ -12,6 +14,7 @@ const emit = defineEmits(['update:dark-mode'])
 </script>
 
 <template lang="pug">
+.version {{ version }}
 .top-bar
   .theme-switch(
     @click="switchTheme"
@@ -31,20 +34,36 @@ const emit = defineEmits(['update:dark-mode'])
   left: 0;
   right: 0;
   height: 48px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
   background: rgba($light-main-bg-color, 0.3);
-  backdrop-filter: blur(6px);
   z-index: 10;
   transition: $switch-mode-transition;
-  @include backdrop-blur(6px, rgba($light-main-bg-color, 0.8), rgba(#fff, 0));
   box-shadow: 0 0 1px rgba($light-main-bg-color, 0.2);
+
+  @include backdrop-blur(6px, rgba($light-main-bg-color, 0.8), rgba(#fff, 0));
+}
+
+// The version can't have a mix blend mode if within the top bar:
+// https://stackoverflow.com/questions/33223603/using-css-mix-blend-mode-with-a-child-of-a-position-fixed-element
+.version {
+  font: 0.8rem monospace;
+  text-align: right;
+  opacity: 0.8;
+  mix-blend-mode: difference;
+  position: fixed;
+  z-index: 11;
+  right: 68px;
+  top: 15px;
 }
 
 .theme-switch {
-  position: absolute;
-  top: 10px;
-  right: 10px;
+  position: relative;
+  margin-left: 10px;
+  margin-right: 10px;
   height: 26px;
-  display: flex;
+  display: inline-flex;
   align-items: center;
   justify-content: space-between;
   padding: 0 2px;
@@ -55,7 +74,6 @@ const emit = defineEmits(['update:dark-mode'])
   cursor: pointer;
   outline: 1px solid transparent;
   transition: 0.3s ease-in-out;
-
 
   &:focus {box-shadow: 0 0 3px 2px #e1a116a3;}
 
@@ -92,12 +110,13 @@ const emit = defineEmits(['update:dark-mode'])
 // --------------------------------------------------------
 .dark {
   .top-bar {
-    @include backdrop-blur(6px, rgba($light-main-bg-color, 0.8), rgba(#fff, 0));
     box-shadow: 0 0 12px rgba($dark-main-bg-color, 0.15);
+    @include backdrop-blur(6px, rgba($light-main-bg-color, 0.8), rgba(#fff, 0));
   }
 
   .theme-switch {
     box-shadow: 1px 1px 3px #403216 inset;
+
     &:focus {outline-color: #b9a273;}
 
     .button {color: rgba(#000, 0.5);}
